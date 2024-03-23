5158251a559206126a9579f8e0c76f4d9e7d25f0
miteshagrawal1 <mitesha392@gmail.com>
2024-03-23 10:55:20 +0530

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import axios from 'axios'
 function App() {
 
   const [url, seturl] = useState("")
-  const handleclick = async ()=>{
-    let res = await axios.get("https://api.memegen.link/images/");
-    let index = Math.floor(Math.random() * res.data.length)
-    console.log(index)
-    seturl(res.data[index].url)
+  const [errormsg, seterrormsg] = useState("")
+  const handleclick = async () => {
+    try {
+      let res = await axios.get("https://api.memegen.link/images/");
+      let index = Math.floor(Math.random() * res.data.length)
+      console.log(index)
+      seturl(res.data[index].url)
+    } catch (error) {
+      seterrormsg(error.message)
+    }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     handleclick()
-  },[])
+  }, [])
 
   return (
     <>
       <div className="container">
         <div className="box">
           <img src={url} alt="" />
+          <p>{errormsg !== "" && <h2>{errormsg}</h2>}</p>
           <button onClick={handleclick}>Generate</button>
         </div>
       </div>
